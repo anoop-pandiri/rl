f3dd1ad880ea14884f27b8058942cba47a3d671d
anoop <pandirianoop6@gmail.com>
2024-09-15 00:30:29 +0530

    initial commit

---
+++ src/main/java/com/anoop/rl/RlApplication.java
@@ -0,0 +1,13 @@
+package com.anoop.rl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anoop/rl/RlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anoop.rl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
